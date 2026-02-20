84fc43703812b7cee51c8b9e11b25a9e964e4141
Luvjeet19 <luvjeet.k.1@gmail.com>
2026-02-20 18:16:02 +0530

    Added Stack module

--- src/stack.cpp
+++ src/stack.cpp
@@ -36,19 +36,46 @@ public:
         }
         std::cout << std::endl;
     }
+
+    void menu() {
+        int choice, value;
+
+        do {
+            std::cout << "\n--- Stack Menu ---\n";
+            std::cout << "1. Push\n";
+            std::cout << "2. Pop\n";
+            std::cout << "3. Top\n";
+            std::cout << "4. Display\n";
+            std::cout << "0. Back to Main Menu\n";
+            std::cout << "Enter choice: ";
+            std::cin >> choice;
+
+            switch (choice) {
+                case 1:
+                    std::cout << "Enter value to push: ";
+                    std::cin >> value;
+                    push(value);
+                    break;
+                case 2:
+                    pop();
+                    break;
+                case 3:
+                    std::cout << "Top: " << top() << std::endl;
+                    break;
+                case 4:
+                    display();
+                    break;
+                case 0:
+                    std::cout << "Returning to main menu...\n";
+                    break;
+                default:
+                    std::cout << "Invalid choice!\n";
+            }
+        } while (choice != 0);
+    }
 };
 
 void runStackModule() {
-    std::cout << "Stack Demo" << std::endl;
     Stack s;
-    s.push(10);
-    s.push(20);
-    s.push(30);
-    s.display();
-
-    std::cout << "Top: " << s.top() << std::endl;
-    
-    s.pop();
-    std::cout << "After pop: ";
-    s.display();
+    s.menu();
 }
